c00b3d1ecaac1f3c4fcab653168e47a369221ab7
mark <markpackham1@gmail.com>
2023-04-10 13:00:23 +0100

    run useEffect with local Storage every time todos list changes
    https://youtu.be/Rh3tobg7hEo?t=2352

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NewTodoForm } from "./NewTodoForm";
 import { TodoList } from "./TodoList";
 import "./styles.css";
 
 export default function App() {
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(title) {
     setTodos((currentTodos) => {
