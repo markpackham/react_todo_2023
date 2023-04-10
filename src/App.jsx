22b8f680aa0e984654a8a6a9ca1323d0f164b497
mark <markpackham1@gmail.com>
2023-04-10 08:19:25 +0100

    add form
    https://youtu.be/Rh3tobg7hEo?t=737

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import "./styles.css";
 
 export default function App() {
-  return "Hello World";
+  return (
+    <form className="new-item-form" action="">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+      </div>
+    </form>
+  );
 }
