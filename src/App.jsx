f2196cc2b1a580b1c6c1f68ef51f427128d94069
mark <markpackham1@gmail.com>
2023-04-10 08:30:04 +0100

    add items with checkbox and button
    https://youtu.be/Rh3tobg7hEo?t=862

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,32 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <form className="new-item-form" action="">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
-      </div>
-    </form>
+    <>
+      <form className="new-item-form" action="">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" name="" id="" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" name="" id="" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
   );
 }
